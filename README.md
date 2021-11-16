cbf952207d016b675ea3a20ff2b7321b2de73dbf
Waleed Said Kamel <waleedsaid014@gmail.com>
2021-11-16 14:33:18 +0200

    finish from redis

--- README.md
+++ README.md
@@ -4,4 +4,17 @@ php artisan migrate:fresh --seed
 
 * queue
 * php artisan queue:work  // default queue
-  php artisan queue:work --queue=Waleed  // if you want to fire queue by name
+* php artisan queue:work --queue=Waleed  // if you want to fire queue by name
+* how to stop queue when running
+
+redis-cli // enter to redis
+
+keys * // get all keys in redis
+set keyName "KeyValue" //how to set key in redis and value on this key
+
+get KeyName //to get value on this keyName 
+
+del keyName // to delete key name and value on this key
+
+  // delete all keys 
+
--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,17 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+use \Illuminate\Support\Facades\Redis;
 
 Route::get('/', function () {
-    return view('welcome');
+    /*for ($i=1; $i < 10; $i++) {
+        Redis::set('m_'.$i,$i);
+    }*/
+    return Redis::get('m_*');
+    Redis::set('w_4','4'); // how to set key and value in redis
+
+    return Redis::get('w_4'); // get value from redis
+   return Redis::keys('w_4');  // get all key this matching
 });
 
 Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
